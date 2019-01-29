21623d094758e044e0d32b3cfb2195a63f1cadaf
Fedor Shatalov <shatalov@uchi.ru>
2019-01-29 13:54:37 +0300

    Added custom routes and links to pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,32 +1,32 @@
 require 'spec_helper'
 
-def page_content(name)
-  case name
-  when "Home"
-    "Sample App"
-  when "Help"
-    "Help"
-  when "About"
-    "About Us"
-  when "Contact"
-    "Contacts"
-  end
-end
-
-def page_test(name)
-  describe "#{name} page" do
-
-    it "should have the content #{page_content(name)}" do
-      visit "/static_pages/#{name.downcase}"
-      expect(page).to have_content(page_content(name))
-    end
-
-    it "should have the right title" do
-      visit "/static_pages/#{name.downcase}"
-      expect(page).to have_title("#{base_title} | #{name}")
-    end
-  end
-end
+# def page_content(name)
+#   case name
+#   when "Home"
+#     "Sample App"
+#   when "Help"
+#     "Help"
+#   when "About"
+#     "About Us"
+#   when "Contact"
+#     "Contacts"
+#   end
+# end
+#
+# def page_test(name)
+#   describe "#{name} page" do
+#
+#     it "should have the content #{page_content(name)}" do
+#       visit "/static_pages/#{name.downcase}"
+#       expect(page).to have_content(page_content(name))
+#     end
+#
+#     it "should have the right title" do
+#       visit "/static_pages/#{name.downcase}"
+#       expect(page).to have_title("#{base_title} | #{name}")
+#     end
+#   end
+# end
 
 describe "StaticPages" do
 
@@ -38,17 +38,17 @@ describe "StaticPages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
@@ -56,12 +56,12 @@ describe "StaticPages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
@@ -69,12 +69,12 @@ describe "StaticPages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About")
     end
   end
@@ -82,12 +82,12 @@ describe "StaticPages" do
   describe "Contact page" do
 
     it "should have the content 'Contacts'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contacts')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contacts")
     end
   end
