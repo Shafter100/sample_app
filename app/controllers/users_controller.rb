f921f2232bd5039e3868205691a7968806427f45
Fedor Shatalov <shatalov@uchi.ru>
2019-02-07 10:36:24 +0300

    Proper signup control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      #redirect_to :controller => StaticPagesController, :action => :home
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
       # Handle a successful save.
     else
       render 'new'
