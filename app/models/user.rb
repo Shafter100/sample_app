ebed76541dc62ab3614daabec9bff49ea5613011
Fedor Shatalov <shatalov@uchi.ru>
2019-02-08 12:38:34 +0300

    User remember_token generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,22 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 
   before_save{ email.downcase! }
+  before_create :create_remember_token
 
   has_secure_password # it allows blank passwords, so there is additional validation below
   validates :password, length: {minimum: 6}, presence: true unless :password.blank?
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
