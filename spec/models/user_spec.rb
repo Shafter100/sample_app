dd101a3749d32b6d25cf144b2f106a51c177f689
Fedor Shatalov <shatalov@uchi.ru>
2019-02-25 09:33:51 +0300

    Working feed of user's microposts on the Home page

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
 
   it { should be_valid }
@@ -147,6 +148,16 @@ describe User do
       microposts.each do |micropost|
         expect(Micropost.where(id: micropost.id)).to be_empty
       end
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryBot.create(:micropost, user: FactoryBot.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
     end
   end
 
